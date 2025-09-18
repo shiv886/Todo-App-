14f14da5f191431505f388f0071062214d026f28
Shivam kumar <151580741+shiv886@users.noreply.github.com>
2025-09-18 07:09:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+/*import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg' */
+import './App.css'
+/*import Counter from "./counter.jsx"*/
+/*import LikeButton from './likeButton.jsx'*/
+/*import BoardGame from './boardGame.jsx'*/
+import Todolist from './toodolist.jsx'
+/*import Lottery from './lottery.jsx'*/
+function App() {
+
+
+  return (
+    <Todolist />
+  )
+}
+
+export default App
---
+++ toodolist.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./todo.css"; // Import CSS file
+
+export default function Todolist() {
+  // Load todos from localStorage (if any), otherwise empty array
+  let getInitialTodos = () => {
+    let savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  };
+
+  let [todos, settodos] = useState(getInitialTodos);
+  let [newTOdo, setnewTodo] = useState("");
+
+  // Save todos to localStorage whenever `todos` changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  let addnewTask = () => {
+    if (newTOdo.trim() === "") return; // prevent empty task
+    settodos([...todos, { id: uuidv4(), task: newTOdo, isDone: false }]);
+    setnewTodo("");
+  };
+
+  let Updatenewtodo = (event) => {
+    setnewTodo(event.target.value);
+  };
+
+  let deletetask = (id) => {
+    let updatedtodos = todos.filter((todo) => todo.id !== id);
+    settodos(updatedtodos);
+  };
+
+  let markasdoneAll = () => {
+    settodos(
+      todos.map((todo) => ({
+        ...todo,
+        isDone: true,
+      }))
+    );
+  };
+
+  let markasdone = (id) => {
+    settodos(
+      todos.map((todo) =>
+        todo.id===id ? { ...todo, isDone: true } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="todo-container">
+      <h2>To-Do App</h2>
+      <input
+        className="todo-input"
+        placeholder="Add a task"
+        value={newTOdo}
+        onChange={Updatenewtodo}
+      />
+      <button className="add-btn" onClick={addnewTask}>
+        Add
+      </button>
+
+      <h4>Task to do</h4>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span style={todo.isDone ? {textDecoration: "line-through"} : {}}>
+              {todo.task}
+            </span>
+            <div>
+              <button onClick={()=>deletetask(todo.id)}>Delete</button>
+              &nbsp;&nbsp;
+               <button onClick={()=>markasdone(todo.id)}>Mark as done</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {todos.length > 0 && (
+        <button className="mark-all-btn" onClick={markasdoneAll}>
+          Mark all as done
+        </button>
+      )}
+    </div>
+  );
+}
+
+
